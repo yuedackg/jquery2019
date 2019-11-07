b62f900990263e4f5f14a56a1a98c7e03b229f9f
Yoshihiro UEDA <cqb00504.00@gmail.com>
2019-11-07 19:20:19 +0900

    201911071920

---
+++ js/script.js
@@ -0,0 +1,72 @@
+function close1234display56(){
+    let item = document.getElementById("sec1");
+    item.style.display="none";
+    item = document.getElementById("sec2");
+    item.style.display="none";
+    item = document.getElementById("sec3");
+    item.style.display="none";
+    item = document.getElementById("sec4");
+    item.style.display="none";
+    item = document.getElementById("sec7");
+    item.style.display="none";
+    item = document.getElementById("sec8");
+    item.style.display="none";
+    item = document.getElementById("sec5");
+    item.style.display="block";
+    item = document.getElementById("sec6");
+    item.style.display="block";
+}
+function close1234display78(){
+    let item = document.getElementById("sec1");
+    item.style.display="none";
+    item = document.getElementById("sec2");
+    item.style.display="none";
+    item = document.getElementById("sec3");
+    item.style.display="none";
+    item = document.getElementById("sec4");
+    item.style.display="none";
+    item = document.getElementById("sec5");
+    item.style.display="none";
+    item = document.getElementById("sec6");
+    item.style.display="none";
+    item = document.getElementById("sec7");
+    item.style.display="block";
+    item = document.getElementById("sec8");
+    item.style.display="block";
+}
+function close56display12(){
+    let item = document.getElementById("sec1");
+    item.style.display="block";
+    item = document.getElementById("sec2");
+    item.style.display="block";
+    item = document.getElementById("sec3");
+    item.style.display="block";
+    item = document.getElementById("sec4");
+    item.style.display="block";
+    item = document.getElementById("sec5");
+    item.style.display="none";
+    item = document.getElementById("sec6");
+    item.style.display="none";
+    item = document.getElementById("sec7");
+    item.style.display="none";
+    item = document.getElementById("sec8");
+    item.style.display="none";
+}
+function close78display12(){
+    let item = document.getElementById("sec1");
+    item.style.display="block";
+    item = document.getElementById("sec2");
+    item.style.display="block";
+    item = document.getElementById("sec3");
+    item.style.display="block";
+    item = document.getElementById("sec4");
+    item.style.display="block";
+    item = document.getElementById("sec5");
+    item.style.display="none";
+    item = document.getElementById("sec6");
+    item.style.display="none";
+    item = document.getElementById("sec7");
+    item.style.display="none";
+    item = document.getElementById("sec8");
+    item.style.display="none";
+}
